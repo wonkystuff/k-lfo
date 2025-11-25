fba1eff7646647c5809fa39c12214d408618ee4e
namke <john@namke.com>
2025-11-25 20:27:37 +0000

    Updates to try and get an unprogrammed ATTiny85 to accept programming

---
+++ flash.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+#if [[ -z $1 ]];
+#then 
+#    echo "No parameter passed."
+#else
+#    echo "Parameter passed = $1"
+    #cd $1
+~/.platformio/penv/bin/platformio run --target upload
+#fi
+
